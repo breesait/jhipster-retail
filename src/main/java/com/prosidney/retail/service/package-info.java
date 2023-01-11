cdc5f46fe0b93771b9cd583c70c7d1b9cb556be5
sidney <prosidney@gmail.com>
2023-01-11 23:22:16 +0100

    Initial version of retail generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/prosidney/retail/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Service layer beans.
+ */
+package com.prosidney.retail.service;
---
+++ src/main/java/com/prosidney/retail/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.prosidney.retail.web.rest.vm;
